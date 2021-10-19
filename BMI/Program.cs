6fd7392726a729ae6df6d5ab73c099606c87311d
Bejček Tomáš <Bejcek.Tomas@stud.vassboskovice.cz>
2021-10-19 08:19:30 +0200

    Add project files.

---
+++ BMI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double height;
+            double weight;
+            double BMI;
+            
+            
+            
+            Console.WriteLine("BMI calculator");
+        }   
+    }
+}
