a000d87ee3376ad78233acec50f4eb83bdd12bc1
Tomáš Bejček <Bejcek.Tomas@stud.vassboskovice.cz>
2021-10-19 09:22:46 +0200

    BMI

--- BMI/Program.cs
+++ BMI/Program.cs
@@ -14,52 +14,51 @@ namespace BMI
             {
                 Console.Clear();
 
-                Console.WriteLine("BMI calculator");
-                Console.Write("Enter weight:");
+                Console.WriteLine(" BMI calculator");
+                Console.Write(" Enter weight:");
                 weight = double.Parse(Console.ReadLine());
-                Console.Write("Enter height:");
+                Console.Write(" Enter height:");
                 height = double.Parse(Console.ReadLine());
                 heightm = height / (100);
 
                 if (height <= 0 || weight <= 0)
                 {
-                    Console.WriteLine("nejses antihmota!");
+                    Console.WriteLine(" nejses antihmota!");
                     break;
                 }
 
 
                 BMI = weight / (heightm * heightm);
-                Console.WriteLine(BMI);
-                BMI = Math.Floor(BMI);
-                
+                BMI = Math.Round(BMI, 1);
+              
                 Console.WriteLine();
-                Console.WriteLine($"Your BMI is {BMI}. ");
+                Console.WriteLine($" Your BMI is {BMI}. ");
                 Console.WriteLine();
                 
                 if (BMI < (18.5))
                 {
-                    Console.WriteLine("You have underweight!");
+                    Console.WriteLine(" You have underweight!");
 
                 }
                 else if (BMI > (18.5) && BMI < (24.9))
                 {
-                    Console.WriteLine("You are in normal!");
+                    Console.WriteLine(" You are in normal!");
                 }
                 else if (BMI > (25) && BMI < (29.9))
                 {
-                    Console.WriteLine("You have overweight!");
+                    Console.WriteLine(" You have overweight!");
                 }
                 else if (BMI > (30) && BMI < (34.9))
                 {
-                    Console.WriteLine("You have 1st degree obesity!");
+                    Console.WriteLine(" You have 1st degree obesity!");
                 }
                 else if (BMI > (35) && BMI < (39.9))
                 {
-                    Console.WriteLine("You have 2nd degree obesity!");
+                    Console.WriteLine(" You have 2nd degree obesity!");
                 }
                 else if (BMI > (40))
                 {
-                    Console.WriteLine("You have 3rd degree obesity!");
+                    Console.WriteLine(" You have 3rd degree obesity!");
                 }
                 
 
